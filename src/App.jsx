a5ba6aeed82342718a7c3549aba8653bf1144e94
Mutlu Kurt <81134748+mutlukurt@users.noreply.github.com>
2025-08-03 16:12:31 +0300

    Add GitHub Actions for automatic build and deploy + Configure for GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import Footer from './components/Footer';
 
 function App() {
   return (
-    <Router>
+    <Router basename="/portfolio-web-site">
       <div className="App">
         <Header />
         <main>
