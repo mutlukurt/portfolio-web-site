51a008fb9a5ac8d82e162d05a2dc682ddccef0b9
Mutlu Kurt <81134748+mutlukurt@users.noreply.github.com>
2025-09-09 17:55:04 +0300

    Fix routing and 404 error

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import Footer from './components/Footer';
 
 function App() {
   return (
-    <Router basename="/portfolio-web-site">
+    <Router>
       <div className="App">
         <Header />
         <main>
--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio-web-site/',
+  base: '/',
   build: {
     outDir: 'build',
     assetsDir: 'static'
   },
   server: {
     port: 3000,
-    host: true
+    host: true,
+    open: true
   }
 })
